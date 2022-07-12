ab90003d0772eb30f79d23d61e6f7a6804c07ed8
Marte Løvoll <martlovo@uio.no>
2022-07-12 12:36:31 +0200

    hei i added two more functions albl  + spher

--- Core/CalculateMie.py
+++ Core/CalculateMie.py
@@ -1,4 +1,4 @@
-from tools.functions import Refractive_Index
+from tools.functions import Refractive_Index, SphericalMie, a_Lb_L, crosss
 from tools.misc import Input
 
 def Calculate_Individual():
@@ -6,3 +6,5 @@ def Calculate_Individual():
     radius, PType, PFiles, MType, MFiles, eVmin, eVmax, wl_step = Input()
     n_particle, k_particle = Refractive_Index(PFiles, eVmin, eVmax, wl_step)
     n_matrix, k_matrix = Refractive_Index(MFiles, eVmin, eVmax, wl_step)
+    a_L, b_L, L = a_Lb_L(n_particle, n_matrix, radius)
+    hei = crosss(a_L, b_L, L, n_particle)

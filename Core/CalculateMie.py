77e29a1fb933c7f9186c967d2b18cd27c550e058
Bothkevin <kevin.both@swissonline.ch>
2022-07-12 07:51:21 +0200

    first commit

---
+++ Core/CalculateMie.py
@@ -0,0 +1,9 @@
+from tools.functions import Refractive_Index
+from tools.misc import Input
+def Calculate_Individual():
+
+    radius, PType, PFiles, MType, MFiles, eVmin, eVmax, wl_step = Input()
+    #n_particle, k_particle = Refractive_Index(PFiles, eVmin, eVmax, wl_step)
+    #n_matrix, k_matrix = Refractive_Index(MFiles, eVmin, eVmax, wl_step)
+    Refractive_Index(PFiles, eVmin, eVmax, wl_step)
+    Refractive_Index(MFiles, eVmin, eVmax, wl_step)
---
+++ main.py
@@ -0,0 +1,7 @@
+from Core.CalculateMie import Calculate_Individual
+
+def main():
+    Calculate_Individual()
+
+if __name__ == '__main__':
+    main()
